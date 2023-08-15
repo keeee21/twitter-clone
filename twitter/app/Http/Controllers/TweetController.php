c7b2e0a2f0094b302848e965e03e2dae7a1d01df
Jun-Jun1120 <j.yamazaki@hajimari.inc>
2023-08-15 13:46:58 +0900

    レビューを基に改修

--- twitter/app/Http/Controllers/TweetController.php
+++ twitter/app/Http/Controllers/TweetController.php
@@ -6,26 +6,30 @@ use Illuminate\Http\Request;
 use App\Models\Tweet;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\TweetRequest;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
 
 class TweetController extends Controller
 {
     /**
      * ツイート一覧を表示
      *
-     * @return \Illuminate\View\View
+     * @return View
      */
-    public function index()
+    public function index():View
     {
-        $tweets = Tweet::orderBy('created_at', 'desc')->get();
+        $tweet = new Tweet();
+        $tweets = $tweet->getTweetAll();
         return view('tweets.index', ['tweets' => $tweets]);
     }
 
     /**
      * ツイート作成画面を表示
      *
-     * @return \Illuminate\View\View
+     * @return View
      */
-    public function create()
+    public function create(): View
     {
         return view('tweets.create');
     }
@@ -33,41 +37,42 @@ class TweetController extends Controller
     /**
      * 新しいツイートを保存
      *
-     * @param \App\Http\Requests\TweetRequest  $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @param TweetRequest  $request
+     * @return RedirectResponse
      */
-    public function store(TweetRequest $request)
+    public function store(TweetRequest $request): RedirectResponse
     {
-        Tweet::createTweet($request->content, Auth::id());
+        $tweet = new Tweet();
+        $tweet->createTweetNew($request->content, Auth::id());
         return redirect()->route('tweets.index');
     }
 
     /**
      * 指定したツイートを表示
      *
-     * @param [int] $id
+     * @param [int] $tweetId
      * @return view
      */
-    public function show($id)
+    public function show($tweetId): View
     {
-        $tweet = Tweet::findOrFail($id);
+        $tweet = new Tweet();
+        $tweet = $tweet->findTweetById($tweetId);
         return view('tweets.show', ['tweet' => $tweet]);
     }
 
     /**
      * 指定したツイートを編集する画面を表示
-     *
-     * @param  [int] $id
-     * @return redirect view
+     * @param [int] $tweetId
      * @return view
      */
-    public function edit($id)
+    public function edit($tweetId): View
     {
-        $tweet = Tweet::findOrFail($id);
+        $tweet = new Tweet();
+        $tweet = $tweet->findTweetById($tweetId);
 
         // ユーザーがこのツイートのオーナーであることを確認する
         if (Auth::id() !== $tweet->user_id) {
-            return redirect()->route('tweets.index')->withErrors('このツイートを編集する権限を持っていません');
+            return redirect()->route('tweets.index');
         }
 
         return view('tweets.edit', ['tweet' => $tweet]);
@@ -76,17 +81,18 @@ class TweetController extends Controller
     /**
      * 指定したツイートを更新
      *
-     * @param  \App\Http\Requests\TweetRequest  $request
-     * @param  [int]  $id
-     * @return \Illuminate\Http\RedirectResponse
+     * @param  TweetRequest  $request
+     * @param  [int]  $tweetId
+     * @return RedirectResponse
      */
-    public function update(TweetRequest $request, $id)
+    public function update(TweetRequest $request, $tweetId):RedirectResponse
     {
-        $tweet = Tweet::findOrFail($id);
+        $tweet = new Tweet();
+        $tweet = $tweet->findTweetById($tweetId);
 
-        // ユーザーがこのツイートのオーナーであることを確認する
+    // ユーザーがこのツイートのオーナーであることを確認する
         if (Auth::id() !== $tweet->user_id) {
-            return redirect()->route('tweets.index')->withErrors('このツイートを編集する権限を持っていません');
+            return redirect()->route('tweets.index');
         }
 
         $tweet->updateTweet($request->content);
@@ -97,18 +103,18 @@ class TweetController extends Controller
      * 指定したツイートを削除
      *
      * @param  [int]$id
-     * @return \Illuminate\Http\RedirectResponse
+     * @return RedirectResponse
      */
-    public function destroy($id)
+    public function destroy($tweetId): RedirectResponse
     {
-        $tweet = Tweet::findOrFail($id);
+        $tweet = new Tweet();
+        $tweet = $tweet->findTweetById($tweetId);
 
-        // ユーザーがこのツイートのオーナーであることを確認する
-        if (Auth::id() !== $tweet->user_id) {
+    // ユーザーがこのツイートのオーナーであることを確認する
+        if (!$tweet->deleteTweet(Auth::id())) {
             return redirect()->route('tweets.index');
         }
 
-        $tweet->delete();
         return redirect()->route('tweets.index')->with('message', 'ツイートが削除されました');
     }
 }
--- twitter/app/Models/Tweet.php
+++ twitter/app/Models/Tweet.php
@@ -5,24 +5,82 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
 class Tweet extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['content', 'user_id'];
 
-    public static function createTweet($content, $userId)
+    /**
+     * 全てのツイートを作成日時の降順で取得
+     *
+     * @return Collection
+     */
+    public function getTweetAll()
     {
-        return self::create([
-            'content' => $content,
-            'user_id' => $userId
-        ]);
+        return $this->orderBy('created_at', 'desc')->get();
     }
 
+    /**
+     * 新しいツイートを作成
+     *
+     * @param string $content
+     * @param int $userId
+     * @return boolean
+     */
+    public function createTweetNew($content, $userId)
+    {
+        $this->content = $content;
+        $this->user_id = $userId;
+        return $this->save();
+    }
+
+    /**
+     * ツイートを更新
+     *
+     * @param string $content 更新する内容
+     * @return boolean 更新成功可否
+     */
     public function updateTweet($content)
     {
         $this->content = $content;
         return $this->save();
     }
 
-    use HasFactory;
+    /**
+     * ツイートIDによってツイートを取得
+     *
+     * @param [int] $tweetId
+     * @return Model
+     */
+    public function findTweetById($tweetId)
+    {
+        return $this->findOrFail($tweetId);
+    }
+
+    /**
+     * ツイートが指定したユーザーによって所有されているかを確認
+     *
+     * @param [int] $userId
+     * @return boolean
+     */
+    public function isOwnedBy($userId)
+    {
+        return $this->user_id == $userId;
+    }
+
+    /**
+     * ユーザーIDに基づいてツイートを削除
+     *
+     * @param [int] $userId
+     * @return boolean
+     */
+    public function deleteTweet($userId)
+    {
+        if ($this->user_id !== $userId) {
+            return false;
+        }
+
+        return $this->delete();
+    }
 }
