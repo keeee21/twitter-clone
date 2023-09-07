b55154dab7162d409d744c0bb5721330af4a309e
JoMashiko <j.mashiko@hajimari.inc>
2023-09-07 15:39:10 +0900

    /users/{id}のGETエンドポイントを作成

--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ユーザー詳細
+Route::get('/user/{id}', [App\Http\Controllers\UserController::class,'findByUserId'])->name('findByUserId');
+
