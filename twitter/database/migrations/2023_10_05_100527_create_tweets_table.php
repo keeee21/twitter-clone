d0003ddc0ad491c953176411789d5e96f97ee2b6
河野康平 <kawanokohei@HajimariKoheiKawano.local>
2023-10-10 15:07:06 +0900

    :update:ジョーからのレビュー修正

--- twitter/database/migrations/2023_10_05_100527_create_tweets_table.php
+++ twitter/database/migrations/2023_10_05_100527_create_tweets_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('user_id');
+            $table->foreignId('user_id')->constrained();
             $table->string('tweet');
             $table->timestamps();
         });
